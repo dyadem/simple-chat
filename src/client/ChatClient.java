4586f9407b9b4c12eb5b4c925c22258b14e174b7
Jake Runzer <jakerunzer@gmail.com>
2017-11-02 00:38:51 -0700

    use sockets

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,42 +1,105 @@
 package client;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.Buffer;
 import java.rmi.*;
 import java.util.*;
 import common.ChatInterface;
 import common.Chat;
 
 public class ChatClient {
-    private static Scanner s;
-    private static final String prompt = "> ";
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedReader stdIn;
 
-    public static void main(String[] argv) {
-        System.setProperty("java.security.policy", "security.policy");
+    private static final int port = 8888;
+    private static final String name = "localhost";
+
+    public ChatClient(String serverName, int serverPort) {
+        System.out.println("Establishing connection...");
 
         try {
-            System.setSecurityManager(new SecurityManager());
-            s = new Scanner(System.in);
-            System.out.print("Enter Your name and press Enter: ");
+            socket = new Socket(serverName, serverPort);
+            System.out.println("Connected: " + socket);
 
-            String name = s.nextLine().trim();
-            ChatInterface client = new Chat(name);
+            start();
+        } catch (UnknownHostException e) {
+            System.out.println("Host unknown: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
-            ChatInterface server = (ChatInterface) Naming.lookup("rmi://localhost/ABC");
-            String msg = "[" + client.getName() + "] got connected";
+        String line = "";
+        while (!line.equals("bye")) {
+            try {
+                System.out.print("> ");
+                line = stdIn.readLine();
+                out.println(line);
 
-            server.send(msg);
-            System.out.println("[System] Chat Remote Object is ready:");
-            server.setClient(client);
-
-            while (true) {
-                System.out.print(prompt);
-                msg = s.nextLine().trim();
-                msg = "[" + client.getName() + "] " + msg;
-                server.send(msg);
+                System.out.println("[server] " + in.readLine());
+            } catch (IOException e) {
+                System.out.println("Sending error: " + e.getMessage());
             }
-
-        } catch (Exception e) {
-            System.out.println("[System] Server failed: " + e);
         }
     }
+
+    public void start() throws IOException {
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void stop() {
+        try {
+            if (in != null) { in.close(); }
+            if (out != null) { out.close(); }
+            if (stdIn != null) { stdIn.close(); }
+            if (socket != null) { socket.close(); }
+        } catch (IOException e) {
+            System.out.println("Error closing...");
+        }
+    }
+
+    public static void main(String args[]) {
+        ChatClient client = new ChatClient(name, port);
+    }
 }
+
+//public class ChatClient {
+//    private static Scanner s;
+//    private static final String prompt = "> ";
+//
+//    public static void main(String[] argv) {
+//        System.setProperty("java.security.policy", "security.policy");
+//
+//        try {
+//            System.setSecurityManager(new SecurityManager());
+//            s = new Scanner(System.in);
+//            System.out.print("Enter Your name and press Enter: ");
+//
+//            String name = s.nextLine().trim();
+//            ChatInterface client = new Chat(name);
+//
+//            ChatInterface server = (ChatInterface) Naming.lookup("rmi://localhost/ABC");
+//            String msg = "[" + client.getName() + "] got connected";
+//
+//            server.send(msg);
+//            System.out.println("[System] Chat Remote Object is ready:");
+//            server.setClient(client);
+//
+//            while (true) {
+//                System.out.print(prompt);
+//                msg = s.nextLine().trim();
+//                msg = "[" + client.getName() + "] " + msg;
+//                server.send(msg);
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println("[System] Server failed: " + e);
+//        }
+//    }
+//}
 
--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,52 +1,112 @@
 package server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.*;
 import java.util.*;
 import common.Chat;
 import common.ChatInterface;
 
 public class ChatServer {
-    private static Scanner s;
-    private static final String prompt = "> ";
 
-    public static void main(String[] argv) {
-        System.setProperty("java.security.policy", "security.policy");
+    private Socket socket;
+    private ServerSocket server;
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedReader stdIn;
 
+    private static final int port = 8888;
+
+    public ChatServer(int port) {
         try {
-            System.setSecurityManager(new SecurityManager());
+            System.out.println("Binding to port " + port);
+            server = new ServerSocket(port);
 
-            // Start RMI registry
-            try {
-                java.rmi.registry.LocateRegistry.createRegistry(1099);
-                System.out.println("RMI registry ready.");
-            } catch (Exception e) {
-                System.out.println("Exception starting RMI registry:");
-                e.printStackTrace();
-                System.exit(0);
-            }
+            System.out.println("Waiting for client...");
+            socket = server.accept();
 
-            s = new Scanner(System.in);
-            System.out.print("Enter Your name and press Enter: ");
-            String name = s.nextLine().trim();
+            System.out.println("Connected to client...");
+            open();
 
-            Chat server = new Chat(name);
-            Naming.rebind("rmi://localhost/ABC", server);
+            boolean done = false;
+            while(!done) {
+                try {
+                    String line = in.readLine();
+                    System.out.println("[client] " + line);
 
-            System.out.println("[System] Chat Remote Object is ready:");
-
-            while (true) {
-                System.out.print(prompt);
-                String msg = s.nextLine().trim();
-
-                if (server.getClient() != null) {
-                    ChatInterface client = server.getClient();
-                    msg = "[" + server.getName() + "] " + msg;
-                    client.send(msg);
+                    System.out.print("> ");
+                    line = stdIn.readLine();
+                    out.println(line);
+                } catch (IOException e) {
+                    done = true;
                 }
             }
-
-        } catch (Exception e) {
-            System.out.println("[System] Server failed: " + e);
+        } catch(IOException e) {
+            System.out.println(e);
         }
     }
+
+    public void open() throws IOException {
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void close() throws IOException {
+        if (in != null) { in.close(); }
+        if (out != null) { out.close(); }
+        if (stdIn != null) {  stdIn.close(); }
+        if (socket != null) { socket.close(); }
+    }
+
+    public static void main(String[] argv) {
+        ChatServer server = new ChatServer(port);
+    }
 }
+
+//public class ChatServer {
+//    private static Scanner s;
+//    private static final String prompt = "> ";
+//
+//    public static void main(String[] argv) {
+//        System.setProperty("java.security.policy", "security.policy");
+//
+//        try {
+//            System.setSecurityManager(new SecurityManager());
+//
+//            // Start RMI registry
+//            try {
+//                java.rmi.registry.LocateRegistry.createRegistry(1099);
+//                System.out.println("RMI registry ready.");
+//            } catch (Exception e) {
+//                System.out.println("Exception starting RMI registry:");
+//                e.printStackTrace();
+//                System.exit(0);
+//            }
+//
+//            s = new Scanner(System.in);
+//            System.out.print("Enter Your name and press Enter: ");
+//            String name = s.nextLine().trim();
+//
+//            Chat server = new Chat(name);
+//            Naming.rebind("rmi://localhost/ABC", server);
+//
+//            System.out.println("[System] Chat Remote Object is ready:");
+//
+//            while (true) {
+//                System.out.print(prompt);
+//                String msg = s.nextLine().trim();
+//
+//                if (server.getClient() != null) {
+//                    ChatInterface client = server.getClient();
+//                    msg = "[" + server.getName() + "] " + msg;
+//                    client.send(msg);
+//                }
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println("[System] Server failed: " + e);
+//        }
+//    }
+//}
