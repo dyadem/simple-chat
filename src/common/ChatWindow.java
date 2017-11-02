bb0de563c33079d44948237e1a2a5f5f1d206917
Jake Runzer <jakerunzer@gmail.com>
2017-11-02 11:40:51 -0700

    styled messages

--- src/common/ChatWindow.java
+++ src/common/ChatWindow.java
@@ -1,28 +1,48 @@
 package common;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
 
 public class ChatWindow {
     public JTextField chatInput;
     private JTextPane chatTextArea;
     public JPanel view;
 
+    private StyledDocument doc;
+    private Style statusStyle;
+    private Style messageStyle;
+    private Style nameStyle;
+
     private String title;
 
     public ChatWindow(String title) {
         this.title = title;
+
+        doc = chatTextArea.getStyledDocument();
+        statusStyle = chatTextArea.addStyle("Status Style", null);
+        StyleConstants.setForeground(statusStyle, Color.GRAY);
+        messageStyle = chatTextArea.addStyle("Message Style", null);
+        StyleConstants.setForeground(messageStyle, Color.BLACK);
+        nameStyle = chatTextArea.addStyle("Name Style", null);
+        StyleConstants.setForeground(nameStyle, Color.MAGENTA);
     }
 
     public void showStatus(String message) {
-        String text = chatTextArea.getText();
-        chatTextArea.setText(text + "\n" + message);
+        appendMessage(message, statusStyle, true);
     }
 
     public void showMessage(String name, String message) {
-        String newText = "[ " + name + " ] " + message;
-        chatTextArea.setText(chatTextArea.getText() + "\n" + newText);
+        appendMessage("[ " + name + " ] ", nameStyle, true);
+        appendMessage(message, messageStyle, false);
+    }
+
+    private void appendMessage(String message, Style style, boolean newline) {
+        try {
+            doc.insertString(doc.getLength(), newline ? "\n" + message : message, style);
+        } catch (BadLocationException e) {}
     }
 }
